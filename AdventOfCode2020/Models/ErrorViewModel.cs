259e0ee1e967486be739c132e47cf656325bc137
Zoook39 <zoook39@gmail.com>
2021-03-22 16:45:13 -0700

    initial commit

---
+++ AdventOfCode2020/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdventOfCode2020.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
