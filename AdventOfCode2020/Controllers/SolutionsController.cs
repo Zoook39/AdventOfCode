ee70656843a7918ae3e7cd5cd4c3437222760e84
Zoook39 <zoook39@gmail.com>
2021-03-22 21:57:38 -0700

    better way of handling days, began designing input form

--- AdventOfCode2020/Controllers/SolutionsController.cs
+++ AdventOfCode2020/Controllers/SolutionsController.cs
@@ -18,107 +18,11 @@ namespace AdventOfCode2020.Controllers
             _logger = logger;
         }
 
-        public IActionResult Day_1(){
-            return View();
+        public IActionResult Solution(int day){
+            SolutionViewModel solutionModel = new SolutionViewModel();
+            solutionModel.day = day;
+            return View(solutionModel);
         }
-
-        public IActionResult Day_2(){
-            return View();
-        }
-
-        public IActionResult Day_3(){
-            return View();
-        }
-
-        public IActionResult Day_4(){
-            return View();
-        }
-
-        public IActionResult Day_5(){
-            return View();
-        }
-
-        public IActionResult Day_6(){
-            return View();
-        }
-
-        public IActionResult Day_7(){
-            return View();
-        }
-
-        public IActionResult Day_8(){
-            return View();
-        }
-
-        public IActionResult Day_9(){
-            return View();
-        }
-
-        public IActionResult Day_10(){
-            return View();
-        }
-
-        public IActionResult Day_11(){
-            return View();
-        }
-
-        public IActionResult Day_12(){
-            return View();
-        }
-
-        public IActionResult Day_13(){
-            return View();
-        }
-
-        public IActionResult Day_14(){
-            return View();
-        }
-
-        public IActionResult Day_15(){
-            return View();
-        }
-
-        public IActionResult Day_16(){
-            return View();
-        }
-
-        public IActionResult Day_17(){
-            return View();
-        }
-
-        public IActionResult Day_18(){
-            return View();
-        }
-
-        public IActionResult Day_19(){
-            return View();
-        }
-
-        public IActionResult Day_20(){
-            return View();
-        }
-
-        public IActionResult Day_21(){
-            return View();
-        }
-
-        public IActionResult Day_22(){
-            return View();
-        }
-
-        public IActionResult Day_23(){
-            return View();
-        }
-
-        public IActionResult Day_24(){
-            return View();
-        }
-
-        public IActionResult Day_25(){
-            return View();
-        }
-
-
-
+      
     }
 }
---
+++ AdventOfCode2020/Models/SolutionViewModel.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace AdventOfCode2020.Models{
+    public class SolutionViewModel{
+        public int day {get;set;}
+        public string input{get;set;}
+        public string output{get;set;}
+    }
+}
