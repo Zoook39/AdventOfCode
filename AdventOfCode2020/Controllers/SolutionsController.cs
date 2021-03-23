36a97df407a1bc61dcae74db930bb944b10fc8a7
Zoook39 <zoook39@gmail.com>
2021-03-22 18:26:15 -0700

    Added solution controller and views

---
+++ AdventOfCode2020/Controllers/SolutionsController.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AdventOfCode2020.Models;
+
+namespace AdventOfCode2020.Controllers
+{
+    public class SolutionsController : Controller
+    {
+        private readonly ILogger<SolutionsController> _logger;
+
+        public SolutionsController(ILogger<SolutionsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Day_1(){
+            return View();
+        }
+
+        public IActionResult Day_2(){
+            return View();
+        }
+
+        public IActionResult Day_3(){
+            return View();
+        }
+
+        public IActionResult Day_4(){
+            return View();
+        }
+
+        public IActionResult Day_5(){
+            return View();
+        }
+
+        public IActionResult Day_6(){
+            return View();
+        }
+
+        public IActionResult Day_7(){
+            return View();
+        }
+
+        public IActionResult Day_8(){
+            return View();
+        }
+
+        public IActionResult Day_9(){
+            return View();
+        }
+
+        public IActionResult Day_10(){
+            return View();
+        }
+
+        public IActionResult Day_11(){
+            return View();
+        }
+
+        public IActionResult Day_12(){
+            return View();
+        }
+
+        public IActionResult Day_13(){
+            return View();
+        }
+
+        public IActionResult Day_14(){
+            return View();
+        }
+
+        public IActionResult Day_15(){
+            return View();
+        }
+
+        public IActionResult Day_16(){
+            return View();
+        }
+
+        public IActionResult Day_17(){
+            return View();
+        }
+
+        public IActionResult Day_18(){
+            return View();
+        }
+
+        public IActionResult Day_19(){
+            return View();
+        }
+
+        public IActionResult Day_20(){
+            return View();
+        }
+
+        public IActionResult Day_21(){
+            return View();
+        }
+
+        public IActionResult Day_22(){
+            return View();
+        }
+
+        public IActionResult Day_23(){
+            return View();
+        }
+
+        public IActionResult Day_24(){
+            return View();
+        }
+
+        public IActionResult Day_25(){
+            return View();
+        }
+
+
+
+    }
+}
